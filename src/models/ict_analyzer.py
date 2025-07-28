164b737d0738f7c9e98dff42ba9991ef3ca52752
daigiatrung2004 <55388468+daigiatrung2004@users.noreply.github.com>
2025-07-28 16:47:51 +0700

    Add ICT analyzer implementation

---
+++ src/models/ict_analyzer.py
@@ -0,0 +1,16 @@
+class ICTAnalyzer:
+    def __init__(self):
+        self.fair_value_gaps = []
+        self.premium_discount_zones = []
+
+    def identify_fvg(self, data):
+        """Identify Fair Value Gaps"""
+        pass
+
+    def find_optimal_entries(self, data):
+        """Find optimal entry points based on ICT concepts"""
+        pass
+
+    def calculate_targets(self, entry_point, direction):
+        """Calculate potential targets using ICT methodology"""
+        pass
